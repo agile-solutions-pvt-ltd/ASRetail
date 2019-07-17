4a78854b227a31479704c55aa990f96e180c735d
nirojhmg <nirojhmg@gmail.com>
2019-07-17 18:36:03 +0545

    Dashboard Sticker,Popup msg on Cash denomination)

--- POS.UI/Controllers/HomeController.cs
+++ POS.UI/Controllers/HomeController.cs
@@ -2,8 +2,10 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using POS.Core;
+using POS.DTO;
 using POS.UI.Models;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 
@@ -24,6 +26,10 @@ namespace POS.UI.Controllers
             ViewData["TotalTransaction"] = _context.SalesInvoice.Count();
             ViewData["TotalQuantity"] = _context.SalesInvoiceItems.Sum(x => x.Quantity);
             ViewData["TotalAmount"] = _context.SalesInvoice.Sum(x => x.Total_Payable_Amount);
+            ViewData["TotalCash"] = _context.TodaySalesInvoicePaymentViewModels.Where(x => x.Trans_Mode=="Cash").Sum(x=>x.TotalAmount);
+            ViewData["TotalCard"] = _context.TodaySalesInvoicePaymentViewModels.Where(x => x.Trans_Mode == "Card").Sum(x => x.TotalAmount);
+            ViewData["TotalCreditNote"] = _context.TodaySalesInvoicePaymentViewModels.Where(x => x.Trans_Mode == "Credit Note").Sum(x => x.TotalAmount);
+
             return View();
         }
 
