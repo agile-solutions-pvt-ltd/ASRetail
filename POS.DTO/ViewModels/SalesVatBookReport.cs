06258dffe83d7a59e685d082e400556a94521766
nirojhmg <nirojhmg@gmail.com>
2019-07-29 10:52:33 +0545

    SalesVatBookReport.cs Class missing

---
+++ POS.DTO/ViewModels/SalesVatBookReport.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace POS.DTO
+{ 
+   public class SalesVatBookReport
+    {
+        public DateTime Trans_Date_AD { get; set; }
+
+        [Key]
+        public string Invoice_Number { get; set; }
+        public string Customer_Name { get; set; }
+        public string Customer_Vat { get; set; }
+        public decimal TaxableAmount { get; set; }
+        public decimal Total_Vat { get; set; }
+        public decimal Total_Net_Amount { get; set; }
+    }
+}
