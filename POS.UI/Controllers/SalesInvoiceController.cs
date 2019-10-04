531df540fec229ec72ca65544ca07c515d7a1df6
ashok subedi <ashok.subedi5@gmail.com>
2019-10-04 12:06:42 +0545

    Fonepay discount amount updated

--- POS.UI/Controllers/SalesInvoiceController.cs
+++ POS.UI/Controllers/SalesInvoiceController.cs
@@ -544,7 +544,7 @@ namespace POS.UI.Controllers
                             assigneduserid = salesInvoice.Created_By,
                             externalDocumentNumber = crNumber,
                             amountrounded = salesInvoice.Total_Net_Amount != salesInvoice.Total_Payable_Amount,
-                            fonepaydiscount = salesInvoice.Trans_Type == "Tax" ? salesInvoice.FonepayDiscountAmount * 113 / 100 : salesInvoice.FonepayDiscountAmount
+                            fonepaydiscount = salesInvoice.Trans_Type == "Tax" ? Convert.ToDecimal(Math.Round(salesInvoice.FonepayDiscountAmount * 113 / 100,2)) : salesInvoice.FonepayDiscountAmount
                             
 
                         };
